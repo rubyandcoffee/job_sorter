50717bf9bf9450aa59221deba590c038ac266939
Alexandra Wolfe <rubyandcoffee@gmail.com>
2016-09-21 13:36:05 +0100

    Added specs for return values

--- lib/job_sorter.rb
+++ lib/job_sorter.rb
@@ -1,5 +1,26 @@
 require "job_sorter/version"
 
 class JobSorter
-  # Your code goes here...
+
+	def initialize
+		@dependencies = Hash.new{|j,d| j[d] = []}
+	end
+
+	def add_job_dependency(job, *job_dependencies)
+		@dependencies[job] = job_dependencies
+	end
+
+	def split_jobs(unsorted_jobs)
+		s_jobs = unsorted_jobs.split(", ")
+		s_jobs.each do |s|
+			k, v = s.split(" => ")
+			@m = add_job_dependency(k,v)
+		end
+	end
+
+  def order_jobs(job_string)
+  	job_sorter = JobSorter.new
+  	job_sorter.split_jobs(job_string)
+  	
+  end
 end
--- spec/job_sorter_spec.rb
+++ spec/job_sorter_spec.rb
@@ -4,47 +4,75 @@ describe JobSorter do
 	describe '.order_jobs' do
 		let(:job_sorter) { JobSorter.new }
 
+		context "given an empty String (no jobs)" do
+			let(:job_string) { " " }
+			it "is of type String" do
+				expect(job_string).to be_a(String)
+			end
+
+			it "returns an empty String" do
+				expect(job_sorter.order_jobs(job_string)).to eql(' ')
+			end
+
+		end
+
 		context "given 'a => '" do
 			let(:job_string) { "a" }
 
-			it "is a string" do
+			it "is of type String" do
 				expect(job_string).to be_a(String)
+			end
+
+			it "returns 'a'" do
+				expect(job_sorter.order_jobs(job_string)).to eq('a')
 			end
 		end
 
 		context "given 'a => , b => , c => '" do
 			let(:job_string) { "a => , b => , c => " }
 
-			it "is a string" do
+			it "is of type String" do
 				expect(job_string).to be_a(String)
+			end
+
+			it "returns 'a b c'" do
+				expect([job_sorter.order_jobs(job_string)]).to eq('a b c')
 			end
 		end
 
 		context "given 'a => , b => c, c => '" do
 			let(:job_string) { "a => , b => c, c => " }
 
-			it "is a string" do
+			it "is of type String" do
 				expect(job_string).to be_a(String)
+			end
+
+			it "returns 'a c b'" do
+				expect(job_sorter.order_jobs(job_string)).to eq('a c b')
 			end
 		end
 
 		context "given 'a => , b => c, c => f, d => a, e => b, f => '" do
 			let(:job_string) { "a => , b => c, c => f, d => a, e => b, f => " }
-			it "is a string" do
+			it "is of type String" do
 				expect(job_string).to be_a(String)
+			end
+
+			it "returns 'a f c b d e'" do
+				expect(job_sorter.order_jobs(job_string)).to eq('a f c b d e')
 			end
 		end
 
 		context "given 'a => , b => , c => c'" do
 			let(:job_string) { "a => , b => , c => c" }
-			it "is a string" do
+			it "is of type String" do
 				expect(job_string).to be_a(String)
 			end
 		end
 
 		context "given 'a => , b => c, c => f, d => a, e => , f => b" do
 			let(:job_string) { "a => , b => c, c => f, d => a, e => , f => b" }
-			it "is a string" do
+			it "is of type String" do
 				expect(job_string).to be_a(String)
 			end
 		end
