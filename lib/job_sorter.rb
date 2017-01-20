9cfdf708537bfc7d15b1ebdfe7d9f7260e5d9ea2
Alexandra Wolfe <rubyandcoffee@gmail.com>
2017-01-20 21:37:35 +0000

    Cleaning up the code (indentation)
    Converted indentation to spaces so that code has proper indentation

--- lib/job_sorter.rb
+++ lib/job_sorter.rb
@@ -1,44 +1,44 @@
 require "tsort"
 class JobSorter
-	include TSort
+  include TSort
 
-	def self_dependency_error(k)
-		raise "Job cannot have self-dependency."
-	end
+  def self_dependency_error(k)
+    raise "Job cannot have self-dependency."
+  end
 
-	def initialize
-		@dependencies = Hash.new{|j,d| j[d] = []}
-	end
+  def initialize
+    @dependencies = Hash.new{ |j,d| j[d] = [] }
+  end
 
-	def add_job_dependency(job, *job_dependencies)
-		@dependencies[job] = job_dependencies
-	end
+  def add_job_dependency(job, *job_dependencies)
+    @dependencies[job] = job_dependencies
+  end
 
-	def tsort_each_node(&block)
-  	@dependencies.each_key(&block)
+  def tsort_each_node(&block)
+    @dependencies.each_key(&block)
   end
 
   def tsort_each_child(job, &block)
-  	@dependencies[job].each(&block) if @dependencies.has_key?(job)
+    @dependencies[job].each(&block) if @dependencies.has_key?(job)
   end
 
-	def split_jobs(unsorted_jobs)
-		s_jobs = unsorted_jobs.split(", ")
-		s_jobs.each do |s|
-			k, v = s.split(" => ")
-			if k === v
-				raise self_dependency_error(k)
-			elsif v === nil
-				@m = add_job_dependency(k)
-			else
-				@m = add_job_dependency(k,v)
-			end
-		end
-	end
+  def split_jobs(unsorted_jobs)
+    s_jobs = unsorted_jobs.split(", ")
+    s_jobs.each do |s|
+      k, v = s.split(" => ")
+      if k === v
+        raise self_dependency_error(k)
+      elsif v === nil
+        @m = add_job_dependency(k)
+      else
+        @m = add_job_dependency(k,v)
+      end
+    end
+  end
 
-	def order_jobs(job_string)
-		job_sorter = JobSorter.new
-		job_sorter.split_jobs(job_string)
-		job_sorter.tsort.join(" ")
-	end
+  def order_jobs(job_string)
+    job_sorter = JobSorter.new
+    job_sorter.split_jobs(job_string)
+    job_sorter.tsort.join(" ")
+  end
 end
